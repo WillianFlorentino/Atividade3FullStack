078760fef08eff712dd41264554e4943c7b8b63c
ThiagoCamponez <thiagotradercamponez@gmail.com>
2024-05-30 11:59:27 -0300

    Consulta e cadastro ok

---
+++ src/services/AtividadeService.js
@@ -0,0 +1,18 @@
+const API_BASE_URL = "http://localhost:3001";
+
+class AtividadeService {
+    async obterTodos() {
+        const response = await fetch(`${API_BASE_URL}/atividade`, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (!response.ok) {
+            console.log('Erro ao obter todas as atividades');
+        }else{
+            const dados = await response.json();
+            return dados;
+        }
+    }
+}
