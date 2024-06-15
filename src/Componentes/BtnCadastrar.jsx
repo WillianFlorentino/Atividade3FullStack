88d9799e5227c45c91151d3695c1283149309ac4
Camponez <thiagocamponez2@gmail.com>
2024-06-15 20:44:58 -0300

    Atualizado

--- src/Componentes/BtnCadastrar.jsx
+++ src/Componentes/BtnCadastrar.jsx
@@ -6,21 +6,22 @@ const BtnCadastrar = ({ editandoAtividade, editandoServico, handleSalvar }) => {
     const [botaoAmarelo, setBotaoAmarelo] = useState(false);
 
     const handleClick = () => {
-        if (typeof handleSalvar === 'function') {
-            setBotaoAmarelo(true);
-            handleSalvar();
-            setTimeout(() => {
-                setBotaoAmarelo(false);
-            }, 300); // Volta para a cor verde depois de 300ms
-        } else {
-            console.error('handleSalvar is not a function');
-        }
+        setBotaoAmarelo(true);
+        handleSalvar();
+        setTimeout(() => {
+            setBotaoAmarelo(false);
+        }, 300); // Volta para a cor verde depois de 300ms
     };
 
     return (
         <>
-            <Button type='submit' className='btn w-100' variant='primary' onClick={handleSalvar}>
-                {editandoAtividade ? <><FaSave /> Salvar</> : <><FaPlus /> Cadastrar</>}
+            <Button
+                type='submit'
+                className='btn w-100'
+                variant={botaoAmarelo ? 'warning' : (editandoAtividade || editandoServico) ? 'warning' : 'success'}
+                onClick={handleClick}
+            >
+                {editandoAtividade || editandoServico ? <><FaSave /> Atualizar</> : <><FaPlus /> Cadastrar</>}
             </Button>
         </>
     );
