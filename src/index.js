5cabaa6d33144390eb47ef20295718c835e49091
ThiagoCamponez <thiagocamponez2@gmail.com>
2024-08-24 10:54:55 -0300

    Atualizações, pronto para começar o desenvolvimento do login

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import './index.css';
-import App from './App';
 import CadAtivSust from './Paginas/AtivSustentaveis/CadAtivSust';
 import Beneficiarios from './Paginas/Beneficiarios/Beneficiarios';
 import FormColab from './Paginas/Colaboradores/FormColab';
